fea4632f8f1b47cb5fe2ca48e7411ac32b997e6b
CareLess <kiae1234@naver.com>
2024-08-21 11:20:23 +0900

    # 초기 프로젝트 설정

---
+++ src/test/java/com/jhp/foryouth/ForyouthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhp.foryouth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForyouthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
