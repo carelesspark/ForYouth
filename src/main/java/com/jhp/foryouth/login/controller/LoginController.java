e1131be25e715527d197342dab41d86c3e4b541f
CareLess <kiae1234@naver.com>
2025-05-15 21:48:58 +0900

    feat: 로그인 관련 LoginController 추가

---
+++ src/main/java/com/jhp/foryouth/login/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.jhp.foryouth.login.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+@Log4j2
+@RequiredArgsConstructor
+public class LoginController {
+
+    @GetMapping("/loginMain")
+    public void loginMain(){
+        log.info("로그인 페이지");
+    }
+
+}
