4ce1a44614ec0d4b4c20088fe413d8a3923873af
CareLess <kiae1234@naver.com>
2025-05-21 04:22:47 +0900

    feat: 선택 약관 체크 값 회원가입 상세 페이지로 전달 및 HTML 반영

--- src/main/java/com/jhp/foryouth/join/controller/JoinController.java
+++ src/main/java/com/jhp/foryouth/join/controller/JoinController.java
@@ -1,10 +1,15 @@
 package com.jhp.foryouth.join.controller;
 
+import com.jhp.foryouth.join.service.JoinService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/join")
@@ -17,9 +22,17 @@ public class JoinController {
         log.info("회원가입 약관 동의 페이지");
     }
 
+    @PostMapping("/joinTermsOfUse_check")
+    public String joinTermsOfUse_check(@RequestParam(defaultValue = "false") boolean agreedEventAlarm, RedirectAttributes redirectAttributes){
+        log.info("선택 체크 값 확인");
+        redirectAttributes.addFlashAttribute("agreedEventAlarm", agreedEventAlarm);
+        return "redirect:/join/joinDetail";
+    }
+
     @GetMapping("/joinDetail")
-    public void joinDetail(){
-        log.info("회원가입 상세 페이지");
+    public void joinDetail(Model model){
+        Boolean agreedEventAlarm = (Boolean)model.getAttribute("agreedEventAlarm");
+        log.info("회원가입 상세 페이지, 동의 여부 : {}", agreedEventAlarm);
     }
 
     @GetMapping("/joinFinish")
