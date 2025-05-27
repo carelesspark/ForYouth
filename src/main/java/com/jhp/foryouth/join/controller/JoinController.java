0cd1c4da32fd512f33c96fc596e7b77d43a8faef
CareLess <kiae1234@naver.com>
2025-05-27 22:08:36 +0900

    feat: 회원가입 컨트롤러 구현 및 예외 처리 추가
    회원가입 컨트롤러 구현 및 예외 처리 추가,
    새로고침 시 사라지는 약관 동의 boolean값에 따른 오류 방지를 위해
    리다이렉트에서 세션으로 저장방식 변경

--- src/main/java/com/jhp/foryouth/join/controller/JoinController.java
+++ src/main/java/com/jhp/foryouth/join/controller/JoinController.java
@@ -1,6 +1,8 @@
 package com.jhp.foryouth.join.controller;
 
 import com.jhp.foryouth.join.service.JoinService;
+import com.jhp.foryouth.user.dto.UserDTO;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
@@ -17,27 +19,50 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequiredArgsConstructor
 public class JoinController {
 
+    private final JoinService joinService;
+
     @GetMapping("/joinTermsOfUse")
-    public void joinTermsOfUse(){
+    public String joinTermsOfUse(){
         log.info("회원가입 약관 동의 페이지");
+
+        return "join/joinTermsOfUse";
     }
 
     @PostMapping("/joinTermsOfUse_check")
-    public String joinTermsOfUse_check(@RequestParam(defaultValue = "false") boolean agreedEventAlarm, RedirectAttributes redirectAttributes){
+    public String joinTermsOfUse_check(@RequestParam(defaultValue = "false") boolean agreedEventAlarm, HttpSession session){
         log.info("선택 체크 값 확인");
-        redirectAttributes.addFlashAttribute("agreedEventAlarm", agreedEventAlarm);
+        session.setAttribute("agreedEventAlarm", agreedEventAlarm);
         return "redirect:/join/joinDetail";
     }
 
     @GetMapping("/joinDetail")
-    public void joinDetail(Model model){
-        Boolean agreedEventAlarm = (Boolean)model.getAttribute("agreedEventAlarm");
+    public String joinDetail(HttpSession httpSession, Model model){
+        Boolean agreedEventAlarm = (Boolean)httpSession.getAttribute("agreedEventAlarm");
         log.info("회원가입 상세 페이지, 동의 여부 : {}", agreedEventAlarm);
+        model.addAttribute("agreedEventAlarm", agreedEventAlarm);
+
+        return "join/joinDetail";
+    }
+
+    @PostMapping("/join_new")
+    public String join_new(UserDTO userDTO, Model model){
+        log.info("회원가입 진행");
+        try {
+            joinService.join(userDTO);
+            return "redirect:/join/joinFinish";
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("error", "예기치 않은 오류가 발생했습니다.");
+            model.addAttribute("error_subtitle", "메인 페이지로 돌아가 다시 시도해주세요:)");
+            return "error/error";
+        }
     }
 
     @GetMapping("/joinFinish")
-    public void joinFinish(){
+    public String joinFinish(HttpSession httpSession){
         log.info("회원가입 완료 페이지");
+        httpSession.removeAttribute("agreedEventAlarm");
+
+        return "join/joinFinish";
     }
 
 }
