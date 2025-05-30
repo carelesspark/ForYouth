5de11f437741c9a8fbb2f5f76c96f66e6ba5b270
CareLess <kiae1234@naver.com>
2025-05-30 23:27:54 +0900

    feat: 비밀번호 암호화 기능 구현

--- src/main/java/com/jhp/foryouth/join/service/impl/JoinServiceImpl.java
+++ src/main/java/com/jhp/foryouth/join/service/impl/JoinServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jhp.foryouth.join.service.impl;
 
+import com.jhp.foryouth.config.PasswordConfig;
 import com.jhp.foryouth.join.repository.CheckIdRepository;
 import com.jhp.foryouth.join.repository.JoinUserAuthRepository;
 import com.jhp.foryouth.join.repository.JoinUserRepository;
@@ -9,6 +10,7 @@ import com.jhp.foryouth.user.domain.UserAuth;
 import com.jhp.foryouth.user.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,7 @@ public class JoinServiceImpl implements JoinService {
     private final JoinUserRepository joinUserRepository;
     private final JoinUserAuthRepository joinUserAuthRepository;
     private final CheckIdRepository checkIdRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     @Override
@@ -31,6 +34,10 @@ public class JoinServiceImpl implements JoinService {
 
         User userEntity = dtoToUserEntity(userDTO);
         joinUserRepository.save(userEntity);
+
+        String pw = userDTO.getUserPw();
+        String encodePw = passwordEncoder.encode(pw);
+        userDTO.setUserPw(encodePw);
 
         UserAuth userAuthEntity = dtoToUserAuthEntity(userEntity, userDTO);
         joinUserAuthRepository.save(userAuthEntity);
