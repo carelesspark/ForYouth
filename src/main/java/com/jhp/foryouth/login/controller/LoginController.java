5a0d125a144536e941271a432f8082f9f8879f5b
CareLess <kiae1234@naver.com>
2025-06-21 01:53:54 +0900

    feat: 사용자 인증 상태에 따른 로그인 페이지 이동 처리

--- src/main/java/com/jhp/foryouth/login/controller/LoginController.java
+++ src/main/java/com/jhp/foryouth/login/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.jhp.foryouth.login.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,12 @@ public class LoginController {
     @GetMapping("/loginMain")
     public String loginMain(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "type", required = false) String type, Model model){
         log.info("로그인 페이지");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated() && !authentication.getPrincipal().equals("anonymousUser")) {
+            return "redirect:/";
+        }
+
         if(error != null) {
             if("oauth2".equals(type)) {
                 model.addAttribute("errorMessage", "간편 로그인 중 오류가 발생했습니다.");
