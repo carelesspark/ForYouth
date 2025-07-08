fbffbb915a93e1e5ace8791f0c8eb562f84f053e
CareLess <kiae1234@naver.com>
2025-07-09 03:11:40 +0900

    feat: 로그인 종류에 따른 관심 분야 저장 서비스 코드 작성

---
+++ src/main/java/com/jhp/foryouth/mypage/service/InterestsService.java
@@ -0,0 +1,5 @@
+package com.jhp.foryouth.mypage.service;
+
+public interface InterestsService {
+    void updateUserInterests(String userId, String provider, String email, String interests);
+}
---
+++ src/main/java/com/jhp/foryouth/mypage/service/impl/InterestsServiceImpl.java
@@ -0,0 +1,85 @@
+package com.jhp.foryouth.mypage.service.impl;
+
+import com.jhp.foryouth.mypage.repository.UserInterestsRepository;
+import com.jhp.foryouth.mypage.repository.UserKakaoInterestsRepository;
+import com.jhp.foryouth.mypage.repository.UserNaverInterestsRepository;
+import com.jhp.foryouth.mypage.service.InterestsService;
+import com.jhp.foryouth.user.domain.*;
+import com.jhp.foryouth.user.repository.*;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@Log4j2
+@RequiredArgsConstructor
+public class InterestsServiceImpl implements InterestsService {
+
+    private final KakaoUserRepository kakaoUserRepository;
+    private final UserKakaoInterestsRepository kakaoInterestsRepository;
+
+    private final NaverUserRepository naverUserRepository;
+    private final UserNaverInterestsRepository naverInterestsRepository;
+
+    private final UserAuthRepository userAuthRepository;
+    private final UserInterestsRepository userInterestsRepository;
+
+    @Transactional
+    @Override
+    public void updateUserInterests(String userId, String provider, String email, String interests) {
+        if(provider == null) {
+            UserAuth userAuth = userAuthRepository.findByUserIdWithUser(userId)
+                    .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
+
+            Optional<UserInterests> existingInterests = userInterestsRepository.findByUserNum(userAuth.getUser().getNum());
+
+            if(existingInterests.isPresent()) {
+                UserInterests userInterests = existingInterests.get();
+                userInterests.setInterests(interests);
+                userInterestsRepository.save(userInterests);
+            } else {
+                UserInterests userInterests = new UserInterests();
+                userInterests.setUser(userAuth.getUser());
+                userInterests.setInterests(interests);
+                userInterestsRepository.save(userInterests);
+            }
+        } else if(provider.equals("naver")) {
+            AuthNaver naver = naverUserRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
+
+            Optional<UserNaverInterests> existingInterests = naverInterestsRepository.findByNaver_Num(naver.getNum());
+
+            if(existingInterests.isPresent()) {
+                UserNaverInterests userNaverInterests = existingInterests.get();
+                userNaverInterests.setInterests(interests);
+                naverInterestsRepository.save(userNaverInterests);
+            } else {
+                UserNaverInterests userNaverInterests = new UserNaverInterests();
+                userNaverInterests.setNaver(naver);
+                userNaverInterests.setInterests(interests);
+                naverInterestsRepository.save(userNaverInterests);
+            }
+        } else if(provider.equals("kakao")) {
+            AuthKakao kakao = kakaoUserRepository.findByEmail(email)
+                    .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
+
+            Optional<UserKakaoInterests> existingInterests = kakaoInterestsRepository.findByKakao_Num(kakao.getNum());
+
+            if(existingInterests.isPresent()) {
+                UserKakaoInterests userKakaoInterests = existingInterests.get();
+                userKakaoInterests.setInterests(interests);
+                kakaoInterestsRepository.save(userKakaoInterests);
+            } else {
+                UserKakaoInterests userKakaoInterests = new UserKakaoInterests();
+                userKakaoInterests.setKakao(kakao);
+                userKakaoInterests.setInterests(interests);
+                kakaoInterestsRepository.save(userKakaoInterests);
+            }
+        }
+    }
+}
