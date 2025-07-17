29c7d93d7daf8a95c6f053159c4eaeae329c37ac
CareLess <kiae1234@naver.com>
2025-07-17 18:22:53 +0900

    feat: 유저 관심분야 레퍼지토리 패키지 구조 변경

--- src/main/java/com/jhp/foryouth/user/repository/UserInterestsRepository.java
+++ src/main/java/com/jhp/foryouth/user/repository/UserInterestsRepository.java
@@ -2,9 +2,17 @@ package com.jhp.foryouth.mypage.repository;
 
 import com.jhp.foryouth.user.domain.UserInterests;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserInterestsRepository extends JpaRepository<UserInterests, Long> {
     Optional<UserInterests> findByUserNum(Long num);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM UserInterests ui WHERE ui.user.num = :userNum")
+    void deleteByUserNum(Long userNum);
 }
