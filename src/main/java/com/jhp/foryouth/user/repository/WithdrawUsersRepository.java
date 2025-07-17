4bd6b8b4784bf7b3440a3235528c74597db172d0
CareLess <kiae1234@naver.com>
2025-07-17 18:33:19 +0900

    feat: 유저 탈퇴 레퍼지토리

---
+++ src/main/java/com/jhp/foryouth/user/repository/WithdrawUsersRepository.java
@@ -0,0 +1,12 @@
+package com.jhp.foryouth.user.repository;
+
+
+import com.jhp.foryouth.user.domain.User;
+import com.jhp.foryouth.user.domain.WithdrawUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface WithdrawUsersRepository extends JpaRepository<WithdrawUsers, Long> {
+
+}
