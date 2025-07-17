444013121e610ec3667a32cdb1c73a68acd0233f
CareLess <kiae1234@naver.com>
2025-07-17 18:32:25 +0900

    feat: 유저 탈퇴 컨트롤러 코드 작성

---
+++ src/main/java/com/jhp/foryouth/mypage/controller/WithdrawController.java
@@ -0,0 +1,50 @@
+package com.jhp.foryouth.mypage.controller;
+
+import com.jhp.foryouth.login.config.CustomUserDetails;
+import com.jhp.foryouth.mypage.dto.WithdrawRequest;
+import com.jhp.foryouth.mypage.service.UserInfoService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class WithdrawController {
+
+    private final UserInfoService userInfoService;
+
+    @PostMapping("/api/user/withdraw")
+    public ResponseEntity<?> withdrawUser(@RequestBody WithdrawRequest request, HttpServletRequest httpRequest) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Object principal = authentication.getPrincipal();
+        String email = null;
+        String userId = authentication.getName();
+        String provider = null;
+
+        if(principal instanceof CustomUserDetails customUserDetails) {
+            email = customUserDetails.getEmail();
+        } else if(authentication instanceof OAuth2AuthenticationToken authenticationToken) {
+            OAuth2User oauth2User = (OAuth2User) principal;
+            email = (String) oauth2User.getAttributes().get("email");
+            provider = authenticationToken.getAuthorizedClientRegistrationId();
+        }
+
+        userInfoService.withdraw(userId, provider, email, request.getReason());
+
+        httpRequest.getSession().invalidate();
+
+        SecurityContextHolder.clearContext();
+
+        return ResponseEntity.ok().body(Map.of("success", true));
+    }
+}
