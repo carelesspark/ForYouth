23efa7c27f807b2f1660ed903c32a014dbfd8087
CareLess <kiae1234@naver.com>
2025-07-22 02:31:18 +0900

    feat: 아이디 중복 체크 컨트롤러 이름 변경 및 이메일 중복 체크 컨트롤러 코드 작성

--- src/main/java/com/jhp/foryouth/join/controller/CheckUserInfoController.java
+++ src/main/java/com/jhp/foryouth/join/controller/CheckUserInfoController.java
@@ -1,6 +1,8 @@
 package com.jhp.foryouth.join.controller;
 
+import com.jhp.foryouth.join.repository.CheckEmailRepository;
 import com.jhp.foryouth.join.repository.CheckIdRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,17 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-public class CheckIdController {
+@RequiredArgsConstructor
+public class CheckUserInfoController {
 
     private final CheckIdRepository checkIdRepository;
+    private final CheckEmailRepository checkEmailRepository;
 
-    public CheckIdController(CheckIdRepository checkIdRepository) {
-        this.checkIdRepository = checkIdRepository;
-    }
-
-    @GetMapping("/check-userid")
+    @GetMapping("/check-userId")
     public Map<String, Boolean> checkUserId(@RequestParam String userId) {
         boolean exists = checkIdRepository.existsByUserId(userId);
+        return Map.of("exists", exists);
+    }
+
+    @GetMapping("/check-userEmail")
+    public Map<String, Boolean> checkUserEmail(@RequestParam String userEmail) {
+        boolean exists = checkEmailRepository.existsByUserEmail(userEmail);
         return Map.of("exists", exists);
     }
 }
