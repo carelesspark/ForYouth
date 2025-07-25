6a987199c7bde397e6013a1c35372da4a76a6f34
CareLess <kiae1234@naver.com>
2025-07-26 00:03:54 +0900

    feat: 마이페이지 내 좋아요 누른 게시글 불러오기 기능 구현

--- src/main/java/com/jhp/foryouth/board/service/FreeBoardService.java
+++ src/main/java/com/jhp/foryouth/board/service/FreeBoardService.java
@@ -3,6 +3,7 @@ package com.jhp.foryouth.board.service;
 import com.jhp.foryouth.board.entity.FreeBoard;
 import com.jhp.foryouth.board.entity.FreeBoardComment;
 import com.jhp.foryouth.mypage.dto.MyComment;
+import com.jhp.foryouth.mypage.dto.MyFavoritePost;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
@@ -11,4 +12,6 @@ public interface FreeBoardService {
     Page<FreeBoard> getPostsByUser(String email, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page);
 
     Page<MyComment> getCommentsByUser(String email, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page);
+
+    Page<MyFavoritePost> getPostsByUserLike(String email, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page);
 }
