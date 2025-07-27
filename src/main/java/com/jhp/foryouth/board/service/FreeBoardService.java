ce9e02f4f4b123f67bbc9422e7af207d5bf358b1
CareLess <kiae1234@naver.com>
2025-07-27 18:16:40 +0900

    feat: 마이페이지 내 좋아요 누른 댓글 불러오기 기능 구현

--- src/main/java/com/jhp/foryouth/board/service/FreeBoardService.java
+++ src/main/java/com/jhp/foryouth/board/service/FreeBoardService.java
@@ -3,6 +3,7 @@ package com.jhp.foryouth.board.service;
 import com.jhp.foryouth.board.entity.FreeBoard;
 import com.jhp.foryouth.board.entity.FreeBoardComment;
 import com.jhp.foryouth.mypage.dto.MyComment;
+import com.jhp.foryouth.mypage.dto.MyFavoriteComment;
 import com.jhp.foryouth.mypage.dto.MyFavoritePost;
 import org.springframework.data.domain.Page;
 
@@ -14,4 +15,6 @@ public interface FreeBoardService {
     Page<MyComment> getCommentsByUser(String email, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page);
 
     Page<MyFavoritePost> getPostsByUserLike(String email, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page);
+
+    Page<MyFavoriteComment> getCommentsByUserLike(String email, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page);
 }
--- src/main/java/com/jhp/foryouth/board/service/impl/FreeBoardServiceImpl.java
+++ src/main/java/com/jhp/foryouth/board/service/impl/FreeBoardServiceImpl.java
@@ -2,11 +2,13 @@ package com.jhp.foryouth.board.service.impl;
 
 import com.jhp.foryouth.board.entity.FreeBoard;
 import com.jhp.foryouth.board.entity.FreeBoardComment;
+import com.jhp.foryouth.board.repository.FreeBoardCommentLikeRepository;
 import com.jhp.foryouth.board.repository.FreeBoardCommentRepository;
 import com.jhp.foryouth.board.repository.FreeBoardPostLikeRepository;
 import com.jhp.foryouth.board.repository.FreeBoardRepository;
 import com.jhp.foryouth.board.service.FreeBoardService;
 import com.jhp.foryouth.mypage.dto.MyComment;
+import com.jhp.foryouth.mypage.dto.MyFavoriteComment;
 import com.jhp.foryouth.mypage.dto.MyFavoritePost;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -28,6 +30,7 @@ public class FreeBoardServiceImpl implements FreeBoardService {
     private final FreeBoardRepository freeBoardRepository;
     private final FreeBoardCommentRepository freeBoardCommentRepository;
     private final FreeBoardPostLikeRepository freeBoardPostLikeRepository;
+    private final FreeBoardCommentLikeRepository freeBoardCommentLikeRepository;
 
     @Override
     public Page<FreeBoard> getPostsByUser(String writerId, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page) {
@@ -89,6 +92,27 @@ public class FreeBoardServiceImpl implements FreeBoardService {
             }
 
             return freeBoardPostLikeRepository.findLikePostByFilter(userId, provider, keyword, startDate, endDate, pageable);
+        }
+    }
+
+    @Override
+    public Page<MyFavoriteComment> getCommentsByUserLike(String userId, String provider, String keyword, LocalDateTime startDate, LocalDateTime endDate, int page) {
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(Sort.Direction.DESC, "regDate"));
+
+        if(provider == null) {
+            provider = "normal";
+
+            if((keyword == null || keyword.isBlank()) && startDate == null && endDate == null) {
+                return freeBoardCommentLikeRepository.findLikeCommentsByUser(userId, provider, pageable);
+            }
+
+            return freeBoardCommentLikeRepository.findLikeCommentsByFilter(userId, provider, keyword, startDate, endDate, pageable);
+        } else {
+            if((keyword == null || keyword.isBlank()) && startDate == null && endDate == null) {
+                return freeBoardCommentLikeRepository.findLikeCommentsByUser(userId, provider, pageable);
+            }
+
+            return freeBoardCommentLikeRepository.findLikeCommentsByFilter(userId, provider, keyword, startDate, endDate, pageable);
         }
     }
 }
