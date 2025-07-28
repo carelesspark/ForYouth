d046626c15d56223ae55849c5e5e246a31c1652d
CareLess <kiae1234@naver.com>
2025-07-28 22:20:06 +0900

    feat: 지역 소식 게시판 엔티티

---
+++ src/main/java/com/jhp/foryouth/board/entity/LocalNewsPost.java
@@ -0,0 +1,43 @@
+package com.jhp.foryouth.board.entity;
+
+import com.jhp.foryouth.global.entity.Base;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class LocalNewsPost extends Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long num;
+
+    private String title;
+
+    private String content;
+
+    private String writerId;
+
+    private String imgUrl;
+
+    private String sourceUrl;
+
+    private LocalDateTime startDate;
+
+    private LocalDateTime endDate;
+
+    private String region;
+
+    private int visitCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_num", referencedColumnName = "num")
+    private SupportCategory supportCategory;
+}
