67e5e09e6b3e79881a6d3db20b330538d305c0d7
CareLess <kiae1234@naver.com>
2025-09-05 17:55:25 +0900

    feat: 유저 즐겨찾기 레퍼지토리 추가

---
+++ src/main/java/com/jhp/foryouth/user/repository/UserBookmarkRepository.java
@@ -0,0 +1,10 @@
+package com.jhp.foryouth.user.repository;
+
+import com.jhp.foryouth.user.entity.UserBookmark;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UserBookmarkRepository extends JpaRepository<UserBookmark, Long> {
+
+}
