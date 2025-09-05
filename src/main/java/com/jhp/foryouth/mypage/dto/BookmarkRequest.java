4652da642bbd88e3a3f1ce358996d6613d5de5bb
CareLess <kiae1234@naver.com>
2025-09-05 17:49:20 +0900

    feat: 북마크 DTO 내 변수 추가

--- src/main/java/com/jhp/foryouth/mypage/dto/BookmarkRequest.java
+++ src/main/java/com/jhp/foryouth/mypage/dto/BookmarkRequest.java
@@ -1,21 +1,28 @@
 package com.jhp.foryouth.mypage.dto;
 
+import com.jhp.foryouth.board.entity.PostType;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class BookmarkRequest {
+    private Long bookmarkId;
     private Long postId;
-    private String title;
+    private PostType postType;
     private String category;
+    private String title;
+    private String writerId;
     private LocalDateTime regDate;
     private Long visitCount;
 
-    public BookmarkRequest(Long postId, String title, String category, LocalDateTime regDate, Long visitCount) {
+    public BookmarkRequest(Long bookmarkId, Long postId, PostType postType, String category, String title, String writerId, LocalDateTime regDate, Long visitCount) {
+        this.bookmarkId = bookmarkId;
         this.postId = postId;
-        this.title = title;
+        this.postType = postType;
         this.category = category;
+        this.title = title;
+        this.writerId = writerId;
         this.regDate = regDate;
         this.visitCount = visitCount;
     }
