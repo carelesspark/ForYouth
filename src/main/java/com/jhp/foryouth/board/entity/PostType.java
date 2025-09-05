c7323f0b3329277f4d3d1dce9936f1b0022ab75b
CareLess <kiae1234@naver.com>
2025-09-05 17:20:11 +0900

    feat: 북마크할 게시글 타입을 정의하는 Enum 생성

---
+++ src/main/java/com/jhp/foryouth/board/entity/PostType.java
@@ -0,0 +1,10 @@
+package com.jhp.foryouth.board.entity;
+
+public enum PostType {
+    EDUCATION,
+    EMPLOYMENT_SUPPORT,
+    FINANCIAL_SUPPORT,
+    HOUSING_SUPPORT,
+    LOCAL_NEWS,
+    WELFARE_BENEFIT
+}
