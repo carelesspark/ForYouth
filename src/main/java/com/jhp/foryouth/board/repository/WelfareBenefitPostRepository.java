d28f8907e65b87e9c83645c3870e93a8cb0e0de5
CareLess <kiae1234@naver.com>
2025-09-08 23:43:32 +0900

    feat: 복지혜택 관련 게시글 레퍼지토리 추가

---
+++ src/main/java/com/jhp/foryouth/board/repository/WelfareBenefitPostRepository.java
@@ -0,0 +1,8 @@
+package com.jhp.foryouth.board.repository;
+
+import com.jhp.foryouth.board.entity.WelfareBenefitPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WelfareBenefitPostRepository extends JpaRepository<WelfareBenefitPost, Long> {
+
+}
