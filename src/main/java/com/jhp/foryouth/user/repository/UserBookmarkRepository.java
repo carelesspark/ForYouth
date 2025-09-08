a8d01e9784d4c71cf908c0ba639d2de80cd91da9
CareLess <kiae1234@naver.com>
2025-09-09 00:03:42 +0900

    feat: Custom Repository를 메인 Repository에 확장 적용

--- src/main/java/com/jhp/foryouth/user/repository/UserBookmarkRepository.java
+++ src/main/java/com/jhp/foryouth/user/repository/UserBookmarkRepository.java
@@ -3,8 +3,6 @@ package com.jhp.foryouth.user.repository;
 import com.jhp.foryouth.user.entity.UserBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-
-public interface UserBookmarkRepository extends JpaRepository<UserBookmark, Long> {
+public interface UserBookmarkRepository extends JpaRepository<UserBookmark, Long>, UserBookmarkRepositoryCustom {
 
 }
