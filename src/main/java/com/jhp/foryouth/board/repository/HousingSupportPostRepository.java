2ae7f787d0c1b4bcd4a2932e469c6d2b7fd08964
CareLess <kiae1234@naver.com>
2025-09-08 23:43:06 +0900

    feat: 주거지원 관련 게시글 레퍼지토리 추가

---
+++ src/main/java/com/jhp/foryouth/board/repository/HousingSupportPostRepository.java
@@ -0,0 +1,8 @@
+package com.jhp.foryouth.board.repository;
+
+import com.jhp.foryouth.board.entity.HousingSupportPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HousingSupportPostRepository extends JpaRepository<HousingSupportPost, Long> {
+
+}
