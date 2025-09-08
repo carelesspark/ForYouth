107079899b135712e50ddea97817473fd2842760
CareLess <kiae1234@naver.com>
2025-09-08 23:42:56 +0900

    feat: 금융지원 관련 게시글 레퍼지토리 추가

---
+++ src/main/java/com/jhp/foryouth/board/repository/FinancialSupportPostRepository.java
@@ -0,0 +1,8 @@
+package com.jhp.foryouth.board.repository;
+
+import com.jhp.foryouth.board.entity.FinancialSupportPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FinancialSupportPostRepository extends JpaRepository<FinancialSupportPost, Long> {
+
+}
