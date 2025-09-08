3f91beea2e501f083de20ba59d9a5e45a596f200
CareLess <kiae1234@naver.com>
2025-09-08 23:42:45 +0900

    feat: 취업지원 관련 게시글 레퍼지토리 추가

---
+++ src/main/java/com/jhp/foryouth/board/repository/EmploymentSupportPostRepository.java
@@ -0,0 +1,8 @@
+package com.jhp.foryouth.board.repository;
+
+import com.jhp.foryouth.board.entity.EmploymentSupportPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmploymentSupportPostRepository extends JpaRepository<EmploymentSupportPost, Long> {
+
+}
