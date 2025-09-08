c77029096c4f491329e8b27d621eaa7609d8d6cc
CareLess <kiae1234@naver.com>
2025-09-08 23:43:19 +0900

    feat: 지역소식 관련 게시글 레퍼지토리 추가

---
+++ src/main/java/com/jhp/foryouth/board/repository/LocalNewsPostRepository.java
@@ -0,0 +1,8 @@
+package com.jhp.foryouth.board.repository;
+
+import com.jhp.foryouth.board.entity.LocalNewsPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocalNewsPostRepository extends JpaRepository<LocalNewsPost, Long> {
+
+}
