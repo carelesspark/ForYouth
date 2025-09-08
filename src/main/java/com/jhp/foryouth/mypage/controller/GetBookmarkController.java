30d0c846a8d41816bd1dd705be3c3bd050af0b13
CareLess <kiae1234@naver.com>
2025-09-08 23:51:17 +0900

    feat: 즐겨찾기 페이지 초기 로딩 컨트롤러 추가

--- src/main/java/com/jhp/foryouth/mypage/controller/GetBookmarkController.java
+++ src/main/java/com/jhp/foryouth/mypage/controller/GetBookmarkController.java
@@ -4,40 +4,66 @@ import com.jhp.foryouth.board.service.BoardTotalService;
 import com.jhp.foryouth.login.config.CustomUserDetails;
 import com.jhp.foryouth.mypage.dto.BookmarkRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
+@Log4j2
 public class GetBookmarkController {
 
     private final BoardTotalService boardTotalService;
 
-    @PostMapping("/api/user/bookmark")
-    public ResponseEntity<Page<BookmarkRequest>> getAllBookmark(@RequestParam(required = false) String category, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
+    @GetMapping("/bookmark")
+    public String myBookmark(@RequestParam(defaultValue = "1") int page, Model model) {
+        log.info("내가 북마크 해놓은 혜택 게시글");
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+            return "login/loginMain";
+        }
 
         Object principal = authentication.getPrincipal();
         String email = null;
         String provider = null;
 
-        if(principal instanceof CustomUserDetails customUserDetails) {
-            email = customUserDetails.getEmail();
-        } else if(authentication instanceof OAuth2AuthenticationToken authenticationToken) {
-            OAuth2User oauth2User = (OAuth2User) principal;
-            email = (String) oauth2User.getAttributes().get("email");
-            provider = authenticationToken.getAuthorizedClientRegistrationId();
+        if(principal instanceof CustomUserDetails customUser) {
+            email = customUser.getEmail();
+        } else if(authentication instanceof OAuth2AuthenticationToken oAuthToken) {
+            OAuth2User oAuth2User = (OAuth2User) principal;
+            email = (String) oAuth2User.getAttributes().get("email");
+            provider = oAuthToken.getAuthorizedClientRegistrationId();
         }
 
-        Page<BookmarkRequest> bookmark = boardTotalService.getBookmarkByUser(email, provider, category, page, size);
+        int zeroBasedPage = Math.max(page - 1, 0);
 
-        return ResponseEntity.ok(bookmark);
+        Page<BookmarkRequest> bookmark = boardTotalService.getBookmarkByUser(email, provider, null, zeroBasedPage);
+
+        long count = bookmark.getTotalElements();
+
+        model.addAttribute("pageTitle", "ForYouth 마이페이지");
+        model.addAttribute("cssPath", "/css/mypage/myBookmark.css");
+        model.addAttribute("jsPath", "/js/mypage/postDate.js");
+
+        model.addAttribute("isLogin", true);
+        model.addAttribute("bookmark", bookmark);
+        model.addAttribute("currentPage", bookmark.getNumber());
+        model.addAttribute("count", count);
+
+        return "mypage/myBookmark";
     }
+
 }
