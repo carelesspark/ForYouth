c24a28290fbb0720f16b0aee1b9954ed4ad13b1a
CareLess <kiae1234@naver.com>
2025-09-09 18:25:41 +0900

    refactor: 한글로 카테고리를 표시하기 위해 Enum 필드 개선

--- src/main/java/com/jhp/foryouth/board/entity/PostType.java
+++ src/main/java/com/jhp/foryouth/board/entity/PostType.java
@@ -1,10 +1,20 @@
 package com.jhp.foryouth.board.entity;
 
 public enum PostType {
-    EDUCATION,
-    EMPLOYMENT_SUPPORT,
-    FINANCIAL_SUPPORT,
-    HOUSING_SUPPORT,
-    LOCAL_NEWS,
-    WELFARE_BENEFIT
+    EDUCATION("교육/자격증"),
+    EMPLOYMENT_SUPPORT("취업지원"),
+    FINANCIAL_SUPPORT("금융지원"),
+    HOUSING_SUPPORT("주거지원"),
+    LOCAL_NEWS("지역소식"),
+    WELFARE_BENEFIT("복지혜택");
+
+    private final String displayName;
+
+    PostType(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
