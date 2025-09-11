439b675e42776a03a90b01ca3efd67d99731b12d
CareLess <kiae1234@naver.com>
2025-09-11 22:21:17 +0900

    feat: 건의사항 전송(저장) 기능 구현

---
+++ src/main/java/com/jhp/foryouth/mypage/service/QuestionService.java
@@ -0,0 +1,5 @@
+package com.jhp.foryouth.mypage.service;
+
+public interface QuestionService {
+    void updateQuestion(String writerId, String provider, String title, String content);
+}
---
+++ src/main/java/com/jhp/foryouth/mypage/service/impl/QuestionServiceImpl.java
@@ -0,0 +1,34 @@
+package com.jhp.foryouth.mypage.service.impl;
+
+import com.jhp.foryouth.board.entity.Question;
+import com.jhp.foryouth.board.entity.QuestionStatus;
+import com.jhp.foryouth.mypage.service.QuestionService;
+import com.jhp.foryouth.user.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@Log4j2
+@RequiredArgsConstructor
+public class QuestionServiceImpl implements QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    @Override
+    public void updateQuestion(String writerId, String provider, String title, String content) {
+        String writerProvider = (provider == null) ? "normal" : provider;
+
+        Question question = Question.builder()
+                        .writerId(writerId)
+                        .writerProvider(writerProvider)
+                        .title(title)
+                        .content(content)
+                        .questionStatus(QuestionStatus.NO)
+                        .build();
+
+        questionRepository.save(question);
+    }
+}
