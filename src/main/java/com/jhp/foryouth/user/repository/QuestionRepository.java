96f55ef20940e53753e44fb11ba9600687b88eff
CareLess <kiae1234@naver.com>
2025-09-18 23:36:55 +0900

    feat: Question과 Answer 데이터 동시 조회 쿼리 추가

--- src/main/java/com/jhp/foryouth/user/repository/QuestionRepository.java
+++ src/main/java/com/jhp/foryouth/user/repository/QuestionRepository.java
@@ -1,10 +1,17 @@
 package com.jhp.foryouth.user.repository;
 
 import com.jhp.foryouth.board.entity.Question;
+import com.jhp.foryouth.mypage.dto.QuestionRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-    Page<Question> findByWriterIdAndWriterProviderOrderByRegDateDesc(String writerId, String writerProvider, Pageable pageable);
+    @Query(value = "SELECT q FROM Question q LEFT JOIN FETCH q.answer WHERE q.writerId = :writerId AND q.writerProvider = :writerProvider",
+            countQuery = "SELECT count(q) FROM Question q WHERE q.writerId = :writerId AND q.writerProvider = :writerProvider")
+    Page<Question> findQuestionsWithAnswers(@Param("writerId") String writerId,
+                                                   @Param("writerProvider") String writerProvider,
+                                                   Pageable pageable);
 }
